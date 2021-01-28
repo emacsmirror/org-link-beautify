6d943dd93e98f1c28ccba5c9e6aa944acdb31bb3
stardiviner <numbchild@gmail.com>
2021-01-28 14:31:00 +0800

    Fix pdfview: link contains page number is string number
    Error during redisplay: (jit-lock-function 1) signaled (wrong-type-argument number-or-marker-p "124")

--- org-link-beautify.el
+++ org-link-beautify.el
@@ -205,7 +205,7 @@ EPUB preview."
   (if (string-match "\\(.*?\\)\\(?:::\\(.*\\)\\)?\\'" path)
       (let* ((file-path (match-string 1 path))
              ;; DEBUG: (_ (lambda (message "--> HERE")))
-             (pdf-page-number (or (match-string 2 path)
+             (pdf-page-number (or (string-to-number (match-string 2 path))
                                   org-link-beautify-pdf-preview-default-page-number))
              (pdf-file (expand-file-name (org-link-unescape file-path)))
              (thumbnails-dir (pcase org-link-beautify-thumbnails-dir
