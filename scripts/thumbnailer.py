4d03391aa6467e193fbaedd5efc37c62ede0d7c7
stardiviner <numbchild@gmail.com>
2024-12-12 20:12:41 +0800

    detect command-line argument number

--- scripts/thumbnailer.py
+++ scripts/thumbnailer.py
@@ -3,9 +3,13 @@
 import sys
 from thumbnail import generate_thumbnail
 
+print(sys.argv)
 input_file = sys.argv[1]
 thumbnail_file = sys.argv[2]
-thumbnail_size = int(sys.argv[3]) or 300
+if len(sys.argv) == 4:
+    thumbnail_size = int(sys.argv[3])
+elif len(sys.argv) == 3:
+    thumbnail_size = 300
 
 options = {
 	'trim': False,
