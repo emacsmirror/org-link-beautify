12e45e22825cee7e37501f67dcea1da36bdcdce6
stardiviner <numbchild@gmail.com>
2025-10-10 17:23:37 +0800

    [webpage] set webpage viewport size: width,height

--- org-link-beautify-new.el
+++ org-link-beautify-new.el
@@ -1603,8 +1603,14 @@ Each element has form (ARCHIVE-FILE-EXTENSION COMMAND)."
   :safe #'listp
   :group 'org-link-beautify)
 
-(defcustom org-link-beautify-offline-webpage-preview-size 800
-  "The webpage offline saved archive file preview screenshot image size."
+(defcustom org-link-beautify-offline-webpage-viewport-width 1920
+  "The webpage offline saved archive file preview screenshot image viewport width."
+  :type 'number
+  :safe #'numberp
+  :group 'org-link-beautify)
+
+(defcustom org-link-beautify-offline-webpage-viewport-height 928
+  "The webpage offline saved archive file preview screenshot image viewport height."
   :type 'number
   :safe #'numberp
   :group 'org-link-beautify)
@@ -1617,7 +1623,7 @@ Each element has form (ARCHIVE-FILE-EXTENSION COMMAND)."
            (offline-webpage-file (expand-file-name (org-link-unescape file-path)))
            (thumbnails-dir (org-link-beautify--get-thumbnails-dir-path offline-webpage-file))
            (thumbnail-file (expand-file-name (format "%s%s.png" thumbnails-dir (file-name-base offline-webpage-file))))
-           (thumbnail-size (or org-link-beautify-offline-webpage-preview-size 600))
+           (thumbnail-size (or org-link-beautify-offline-webpage-viewport-width 600))
            (proc-name (format "org-link-beautify offline webpage preview - %s" offline-webpage-file))
            (proc-buffer (format " *org-link-beautify offline webpage preview - %s*" offline-webpage-file))
            (proc (get-process proc-name)))
@@ -1634,7 +1640,8 @@ Each element has form (ARCHIVE-FILE-EXTENSION COMMAND)."
                 org-link-beautify-offline-webpage-preview-command
                 "--headless"
                 (format "--screenshot=%s" thumbnail-file)
-                (format "--window-size=%s" "1920,1080")
+                ;; web browser viewport: width,height: "1920,928"
+                (format "--window-size=%s,%s" org-link-beautify-offline-webpage-viewport-width org-link-beautify-offline-webpage-viewport-height)
                 "--hide-scrollbars"
                 offline-webpage-file)))))
         (when (and org-link-beautify-enable-debug-p (not (file-exists-p thumbnail-file)))
